db58f37d56371f8b90f331e2eccc77e912a938ac
MonMaramba <monmaramba@yahoo.com>
2022-10-08 16:52:00 -0700

    convered App to tsx. Search functions as expected

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import './search-box.styles.css';
 
 
-const SearchBox = ({className, placeholder, onChangHandler }) => (
+// interfaces can be overloaded and extended like classes
+// interface ISearchBoxProps {
+//   className: string;
+//   placeholder?: string | null;
+ 
+// }
+
+// interface ISearchBoxProps {
+//   onChangeHandler: (a:string) => void
+// }
+
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler:(event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBox = ({className, placeholder, onChangeHandler }: SearchBoxProps) => (
   <input
     className={'search-box ${className}'}
     type='search'
     placeholder={placeholder}
-    onChange={(e)=> onChangHandler(e)}
+    onChange={onChangeHandler}
   />
 );
 
